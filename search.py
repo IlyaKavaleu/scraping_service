bc01b282b52a9e08ccbccb16c7c9f2576b5f977d
ilia <kavaleuilia@gmail.com>
2024-02-25 20:03:19 +0100

    change json script

--- search.py
+++ search.py
@@ -1,32 +1,32 @@
 import json
-#
-# with open('newdata.json', 'r', encoding='utf-8') as f:
-#     dates = json.load(f)
-#     new = []
-#     li = []
-#
-alpha = [
-        'б'.title() or 'в'.title() or 'г'.title() or 'д'.title() or 'ж'.title() or 'з'.title() or 'й'.title() or 'к'.title() or
-        'л'.title() or 'м'.title() or 'н'.title() or 'п'.title() or 'p'.title() or 'c'.title() or 'т'.title() or 'ф'.title() or 'x'.title() or 'ц'.title() or 'ч'.title() or 'ш'.title() or 'щ'.title() or
-        'a'.title() or 'y'.title() or 'o'.title() or 'ы'.title() or 'э'.title() or 'я'.title() or 'ю'.title() or 'ё'.title() or 'и'.title() or 'e'.title()]
-#
-#     for data in dates:
-#         new.append([str(data).replace(char, '') for char in alpha if char in str(data)])
-#
-#         if str(data).count('Ч'):
-#             li.append(str(data).replace('Ч', ''))
-#         else:
-#             li.append(data)
 
-with open('suodata.json', 'r', encoding='utf-8') as f:
-    dates = json.load(f)
-    for date in dates:
-        print(date)
-#     for data in dates:
-#         print(data)
-# for data in dates:
-#     if str(data).count('Ч'):
-#         data = str(data).replace('Ч', '')
-#
-# with open('newdata.json', 'w', encoding='utf-8') as file:
-#     json.dump(dates, file, ensure_ascii=False, indent=4)
+
+# def has_cyrillic(text):
+#     # Проверяем, содержит ли текст кириллические символы
+#     data = any(ord(char) > 127 for char in text)
+#     del data
+
+def remove_cyrillic(json_data):
+    if isinstance(json_data, dict):
+        for key, value in list(json_data.items()):
+            if isinstance(value, str):
+                new_value = ''.join([char for char in value if ord(char) <= 127])
+                print(new_value)
+                json_data[key] = new_value
+            elif isinstance(value, (list, dict)):
+                remove_cyrillic(value)
+    elif isinstance(json_data, list):
+        for item in json_data:
+            remove_cyrillic(item)
+
+
+# Загрузка JSON из файла
+with open("data.json", "r", encoding="utf-8") as file:
+    json_data = json.load(file)
+
+# Проверяем и удаляем кириллические символы
+remove_cyrillic(json_data)
+
+# Сохраняем обновленный JSON
+with open("data.json", "w", encoding="utf-8") as file:
+    json.dump(json_data, file, ensure_ascii=False, indent=4)
